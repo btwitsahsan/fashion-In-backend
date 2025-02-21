fe7d84967fbff040eb334eb18222efc26fa756d2
btwitsahsan <ahsan899289@gmail.com>
2025-02-21 08:24:24 +0500

    f

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
 app.use(
     cors({
-      origin: ["http://localhost:3000","https://chat-web-frontend-rho.vercel.app"],
+      origin: ["http://localhost:3000","https://fashion-in-frontend.vercel.app"],
       credentials: true, // ✅ Required to send cookies
       allowedHeaders: ["Content-Type", "Authorization"],
       methods: ["GET", "POST", "PUT", "DELETE"],
